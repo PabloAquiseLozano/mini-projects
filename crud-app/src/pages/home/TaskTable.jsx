4960582d07bc9f79817b786017b96cdde43dd596
PabloAquiseLozano <aquise.pablo@iestpgildaballivian.edu.pe>
2024-10-08 23:44:00 -0500

    no salio

--- crud-app/src/pages/home/TaskTable.jsx
+++ crud-app/src/pages/home/TaskTable.jsx
@@ -2,20 +2,29 @@ import React from "react";
 import { Table } from "antd";
 
 export const TaskTable = ({ task }) => {
-  const columns = [
-    {
-      title: "Tarea a Realizar",
-      dataIndex: "task",
-      key: "task",
-    },
-    {
-      title: "Opciones",
-      dataIndex: "options",
-      key: "options",
-    },
-  ];
+	const columns = [
+		{
+			title: "Tarea a Realizar",
+			dataIndex: "task",
+			key: "task",
+		},
+		{
+			title: "Opciones",
+			dataIndex: "options",
+			key: "options",
+		},
+	];
 
-  return (
-    <Table style={{ width: "70%" }} columns={columns} pagination={false} />
-  );
+	const data = (task, index) => {
+		task.map((tas) => tas.task);
+	};
+
+	return (
+		<Table
+			style={{ width: "70%" }}
+			columns={columns}
+			dataSource={data}
+			pagination={false}
+		/>
+	);
 };
